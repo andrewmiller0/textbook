6a7d662a7418b022d2ecc6372d3c1fcc9d57a44a
melissa <melissaomoy@gmail.com>
2015-02-02 12:15:53 -0500

    created files

---
+++ client/app/account/settings/addClassroom/studentForm/studentForm.directive.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe('Directive: studentForm', function () {
+
+  // load the directive's module and view
+  beforeEach(module('textbookApp'));
+  beforeEach(module('app/account/settings/addClassroom/studentForm/studentForm.html'));
+
+  var element, scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<student-form></student-form>');
+    element = $compile(element)(scope);
+    scope.$apply();
+    expect(element.text()).toBe('this is the studentForm directive');
+  }));
+});
