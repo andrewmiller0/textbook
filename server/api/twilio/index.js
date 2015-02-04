5994498eeceddb6b2cba60daf244582de071cf5e
Chuck Pierce <chuckmpierce@gmail.com>
2015-02-04 10:59:57 -0500

    created logic to get conversation

--- server/api/twilio/index.js
+++ server/api/twilio/index.js
@@ -1,14 +1,26 @@
 var express = require('express'),
 	twilio = require('twilio'),
-	config = require('../../config/environment');
+	config = require('../../config/environment'),
+	Conversation = require('../conversation/conversation.model'),
+	User = require('../user/user.model'),
+	Contact = require('../contact/contact.model')
 
 module.exports = function(req, res) {
 	console.log(req.body);
-	if (twilio.validateExpressRequest(req, config.twilio.clientToken, {url: config.twilio.smsWebhook})) {
-		console.log(req.body);
-		res.header('Content-Type', 'text/xml');
-        var body = req.param('Body').trim();
-        console.log(body);
-        res.send(200);
-	}
+	// if (twilio.validateExpressRequest(req, config.twilio.clientToken, {url: config.twilio.smsWebhook})) {
+	// 	console.log(req.body);
+	// 	res.header('Content-Type', 'text/xml');
+ //        var body = req.param('Body').trim();
+ //        console.log(body);
+ //        res.send(200);
+	// }
+	User.findOne({phone: req.body.To}, function(err, user) {
+		Contact.findOne({phone: req.body.From}, function(err, contact) {
+			Conversation.findOne({userId: user._id, contactId: contact._id}, function(err, conversation) {
+				console.log(conversation);
+			})
+		})
+	});
+	res.send(200);
+
 };
