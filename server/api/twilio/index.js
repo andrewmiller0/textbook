7d19077a299265a70d66eae4c0a2ac8dfdf6abf8
Chuck Pierce <chuckmpierce@gmail.com>
2015-02-04 11:11:34 -0500

    created save for conversation with new message

--- server/api/twilio/index.js
+++ server/api/twilio/index.js
@@ -7,6 +7,7 @@ var express = require('express'),
 
 module.exports = function(req, res) {
 	console.log(req.body);
+	var text = req.body;
 	// if (twilio.validateExpressRequest(req, config.twilio.clientToken, {url: config.twilio.smsWebhook})) {
 	// 	console.log(req.body);
 	// 	res.header('Content-Type', 'text/xml');
@@ -14,13 +15,22 @@ module.exports = function(req, res) {
  //        console.log(body);
  //        res.send(200);
 	// }
-	User.findOne({phone: req.body.To}, function(err, user) {
-		Contact.findOne({phone: req.body.From}, function(err, contact) {
+	var newMessage = {
+		body: text.Body,
+		dateSent: new Date(),
+		type: 'received'
+	};
+	User.findOne({phone: text.To}, function(err, user) {
+		Contact.findOne({phone: text.From}, function(err, contact) {
 			Conversation.findOne({userId: user._id, contactId: contact._id}, function(err, conversation) {
-				console.log(conversation);
+				console.log('found conversation', conversation);
+				conversation.messages.push(newMessage);
+				conversation.unreadMessages = true;
+				conversation.save(function(err, conversation2) {
+					console.log('saved conversation', conversation2);
+					res.send(200);
+				})
 			})
 		})
 	});
-	res.send(200);
-
 };
