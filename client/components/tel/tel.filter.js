718eb60d821d66400d9d03cfd73f96ca56121ac9
Chuck Pierce <chuckmpierce@gmail.com>
2015-02-05 16:52:50 -0500

    changed tel filter and used it for contact numbers

--- client/components/tel/tel.filter.js
+++ client/components/tel/tel.filter.js
@@ -6,9 +6,13 @@ angular.module('textbookApp')
       if (!tel) { return ''; }
 
       var city, number;
-
+      if(tel.length === 10) {
+      	city = tel.slice(0,3);
+      	number = tel.slice(3,6) + '-' + tel.slice(6);
+      } else {
       city = tel.slice(2,5);
-      number = tel.slice(5,8) + '-' + tel.slice(8);
+      number = tel.slice(5,8) + '-' + tel.slice(8);	
+      }
       return ' (' + city + ') ' + number;
     };
   });
