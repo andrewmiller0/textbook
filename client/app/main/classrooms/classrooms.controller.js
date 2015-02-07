5f1357dadfddd25c7648facfa96f82e46ff0e6d3
melissa <melissaomoy@gmail.com>
2015-02-06 19:27:33 -0500

    commented out

--- client/app/main/classrooms/classrooms.controller.js
+++ client/app/main/classrooms/classrooms.controller.js
@@ -34,20 +34,20 @@ angular.module('textbookApp')
 
     socket.socket.on('conversation:save', function(convo){
       // OKAY NO I SHOULD NOT HAVE TO DO THIS.  WHY IS THIS A THING.
-      $scope.classrooms.forEach(function(classroom) {
-        Classroom.get({id: classroom._id}, function(retrievedClassroom) {
-          retrievedClassroom.$promise.then(function(deal) {
-            deal.students.forEach(function(student){
-              student.contacts.forEach(function(retrievedContactId) {
-                console.log(retrievedContactId, convo.contactId)
-                if (retrievedContactId == convo.contactId && $stateParams.className !== deal._id) {
-                  console.log($stateParams, deal._id);
-                  angular.element("#"+deal._id).html(deal.name + '<span class="glyphicon glyphicon-asterisk"></span>');
-                }
-              });
-            });
-          });
-        });
-      });
+      // $scope.classrooms.forEach(function(classroom) {
+      //   Classroom.get({id: classroom._id}, function(retrievedClassroom) {
+      //     retrievedClassroom.$promise.then(function(deal) {
+      //       deal.students.forEach(function(student){
+      //         student.contacts.forEach(function(retrievedContactId) {
+      //           console.log(retrievedContactId, convo.contactId)
+      //           if (retrievedContactId == convo.contactId && $stateParams.className !== deal._id) {
+      //             console.log($stateParams, deal._id);
+      //             angular.element("#"+deal._id).html(deal.name + '<span class="glyphicon glyphicon-asterisk"></span>');
+      //           }
+      //         });
+      //       });
+      //     });
+      //   });
+      // });
     });
 });
