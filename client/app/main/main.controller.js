d8eda55f1c839f765d8be5a0fe356ec911eab526
melissa <melissaomoy@gmail.com>
2015-02-06 19:39:58 -0500

    removed unnecessary thing

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -10,13 +10,4 @@ angular.module('textbookApp')
       	$location.path('/classrooms');
       }
     });
-
-    // $http.put('/api/conversations', {id: }).success(function(result) {
-    // 	socket.socket.on('contact:save', function(a, b, c, d) {
-    // 		console.log(a);
-    // 		console.log(b);
-    // 		console.log(c);
-    // 		console.log(d);
-    // 	});
-    // });
   });
