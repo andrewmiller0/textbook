eaa1d0e5888e532c843ab35222350d28bf1f50e3
Chuck Pierce <chuckmpierce@gmail.com>
2015-02-10 13:00:50 -0500

    added file formats, got rid of some console.logs

--- client/app/main/newClassroom/studentRoster/studentRoster.controller.js
+++ client/app/main/newClassroom/studentRoster/studentRoster.controller.js
@@ -9,23 +9,19 @@ angular.module('textbookApp')
   	$scope.columnNames = Object.keys($scope.studentRoster[0]);
   	$scope.modelNames = {'firstName':'first name', 'lastName': 'last name', 'name': 'primary contact\'s name', 'phone': 'primary contact\'s phone number', 'relationship': 'relation to primary contact'};
   	$scope.columnSelected = {};
-  	console.log($scope.columnNames);
     angular.forEach($scope.studentRoster, function(column) {
     	$scope.rows.push(column);
     });
 
     $scope.changeDataKey = function(model) {
-    	console.log($scope.columnSelected);
     	$scope.studentRoster.forEach(function(column) {
     		for(var key in column) {
     			if(key === $scope.columnSelected[model]) {
-    				console.log($scope.columnSelected[model])
     				column[model] = column[key];
     				delete column[key]; 
     			}
     		}
     	});
-    	console.log($scope.studentRoster[0]);
     };
 
     $scope.createClass = function() {
@@ -41,7 +37,6 @@ angular.module('textbookApp')
     $scope.saveData = function() {
     	$http.post('/api/classrooms/'+ $scope.newClass._id +'/saveSpreadsheet', $scope.studentRoster)
     	.success(function(user) {
-    		console.log(user);
         Auth.updateUser(user);
     		$scope.$emit('updated user');
     		$state.go('classrooms.classroom', {classId: user.classrooms[user.classrooms.length-1]._id});
