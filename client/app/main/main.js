cb6ece4c96b4a402a4c13cad17e59c0164fdea67
Chuck Pierce <chuckmpierce@gmail.com>
2015-02-11 11:21:19 -0500

    create about route

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -6,31 +6,42 @@ angular.module('textbookApp')
       .state('classrooms', {
         url: '/',
         templateUrl: 'app/main/classrooms/classrooms.html',
-        controller: 'ClassroomsCtrl'
+        controller: 'ClassroomsCtrl',
+        authenticate: true
+      })
+      .state('about', {
+        url: '/',
+        templateUrl: 'app/about/about.html',
+        controller: 'AboutCtrl'
       })
       .state('classrooms.edit', {
         url: ':classId/edit',
         templateUrl: 'app/main/editClassroom/editClassroom.html',
-        controller: 'EditClassroomCtrl'
+        controller: 'EditClassroomCtrl',
+        authenticate: true
       })
       .state('addClassroom', {
         url: 'classroom/add',
         templateUrl: 'app/main/newClassroom/newclassroom.html',
-        controller: 'NewClassCtrl'
+        controller: 'NewClassCtrl',
+        authenticate: true
       })
       .state('addClassroom.studentRoster', {
         url: 'classroom/add/roster',
         templateUrl: 'app/main/newClassroom/studentRoster/studentRoster.html',
-        controller: 'StudentRosterCtrl'
+        controller: 'StudentRosterCtrl',
+        authenticate: true
       })
       .state('classrooms.classroom', {
         url: ':classId',
         templateUrl: 'app/main/classroom/classroom.html',
-        controller: 'ClassroomCtrl'
+        controller: 'ClassroomCtrl',
+        authenticate: true
       })
       .state('classrooms.classroom.conversation', {
         url: '/conversations/:contactId',
         templateUrl: 'app/main/classroom/conversation/conversation.html',
-        controller: 'ConversationCtrl'
+        controller: 'ConversationCtrl',
+        authenticate: true
       })
   });
