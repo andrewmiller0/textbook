d7a4d81ed2ff51cca9006576177e434958fcb2b5
Chuck Pierce <chuckmpierce@gmail.com>
2015-02-15 17:38:39 -0500

    wrapped navctrl around everything so that both navs can share...don't know if this is a good idea or not....

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -6,6 +6,7 @@ angular.module('textbookApp')
     Auth.getCurrentUser().$promise.then(function(user) {
       $scope.user = user;
       $scope.selectedClassroom = $scope.user.classrooms[0];
+      console.log('change');
     });
     $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
@@ -21,12 +22,36 @@ angular.module('textbookApp')
       return route === $location.path();
     };
 
+    $scope.showClassroom = function() {
+      console.log($scope.selectedClassroom);
+    }
+
     $scope.setClassroomDropdown = function(classId) {
       angular.forEach($scope.user.classrooms, function(classroom) {
           if(classroom._id === classId) {
             $scope.selectedClassroom = classroom;
           }
         });
+      console.log($scope.selectedClassroom);
+    };
+
+     $scope.deleteClassroom = function(classroomId) {
+      Classroom.delete({id: classroomId});
+      $scope.user.classrooms.forEach(function(classroom, i) {
+        if(classroom._id === classroomId) {
+          $scope.user.classrooms.splice(i, 1);
+          var unpopulatedUser = angular.copy($scope.user);
+          unpopulatedUser.classrooms = unpopulatedUser.classrooms.map(function(classroom) {return classroom._id});
+          User.update({id: $scope.user._id}, unpopulatedUser, function(user) {
+            if($scope.user.classrooms.length === 0) {
+              $state.go('classrooms');
+            } else {
+              $scope.selectedClassroom = $scope.user.classrooms[0];
+              $state.go('classrooms.classroom', {classId: $scope.user.classrooms[0]._id});
+            }
+          });
+        }
+      });
     };
 
   });
