2eb5e7b899704691364fb6846b4eeaba19a87caf
Chuck Pierce <chuckmpierce@gmail.com>
2015-02-15 23:55:25 -0500

    got rid of redundant buttons

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -41,26 +41,7 @@ angular.module('textbookApp')
           }
         });
     };
-
-     $scope.deleteClassroom = function(classroomId) {
-      Classroom.delete({id: classroomId});
-      $scope.user.classrooms.forEach(function(classroom, i) {
-        if(classroom._id === classroomId) {
-          $scope.user.classrooms.splice(i, 1);
-          var unpopulatedUser = angular.copy($scope.user);
-          unpopulatedUser.classrooms = unpopulatedUser.classrooms.map(function(classroom) {return classroom._id});
-          User.update({id: $scope.user._id}, unpopulatedUser, function(user) {
-            if($scope.user.classrooms.length === 0) {
-              $state.go('classrooms');
-            } else {
-              $scope.selectedClassroom = $scope.user.classrooms[0];
-              $state.go('classrooms.classroom', {classId: $scope.user.classrooms[0]._id});
-            }
-          });
-        }
-      });
-    };
-
+    
     $scope.openGroupMsg = function() {
         $scope.groupMessager = $modal.open({
             templateUrl: 'app/main/classrooms/groupMessager/messagemodal.html',
